b2e75d6dcdfc2e671dc442db7611267f901e2fbe
Ben Kirby and Paige Williams <ben@>
2018-11-26 11:14:29 -0800

    Added UI that calls dimensions and weight and displays the cost.

--- app.rb
+++ app.rb
@@ -3,3 +3,19 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/project')
 require('pry')
+
+get("/") do
+  erb(:input)
+end
+
+get ("/output") do
+  @height = params.fetch("height")
+  @width = params.fetch("width")
+  @length = params.fetch("length")
+  @weight = params.fetch("weight")
+
+  parcel = Parcel.new(@height, @width, @length, @weight)
+
+  @cost = parcel.cost_to_ship
+  erb(:output)
+end
