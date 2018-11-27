944d9391ba43858967656f111242d8d4bb210aa5
Ben Kirby and Katrina Hockman <katrina@>
2018-11-27 10:27:53 -0800

    It will now include clues for wrong answers

--- app.rb
+++ app.rb
@@ -12,8 +12,18 @@ post ("/output1") do
   @answer = params.fetch("riddle1")
 
   questions = Sphynx.new(@answer)
- if questions.riddle_one == true
-  erb(:riddle2)
+  if questions.riddle_one == true
+    erb(:riddle2)
+  else erb(:clue1)
+  end
+end
+
+post ("/clue1") do
+  @answer = params.fetch("riddle1")
+
+  questions = Sphynx.new(@answer)
+  if questions.riddle_one == true
+    erb(:riddle2)
   else erb(:fail)
   end
 end
@@ -22,8 +32,18 @@ post ("/output2") do
   @answer = params.fetch("riddle2")
 
   questions = Sphynx.new(@answer)
- if questions.riddle_two == true
-  erb(:riddle3)
+  if questions.riddle_two == true
+    erb(:riddle3)
+  else erb(:clue2)
+  end
+end
+
+post ("/clue2") do
+  @answer = params.fetch("riddle2")
+
+  questions = Sphynx.new(@answer)
+  if questions.riddle_two == true
+    erb(:riddle3)
   else erb(:fail)
   end
 end
@@ -32,9 +52,18 @@ post ("/output3") do
   @answer = params.fetch("riddle3")
 
   questions = Sphynx.new(@answer)
- if questions.riddle_three == true
-  erb(:output)
+  if questions.riddle_three == true
+    erb(:output)
+  else erb(:clue3)
+  end
+end
+
+post ("/clue3") do
+  @answer = params.fetch("riddle3")
+
+  questions = Sphynx.new(@answer)
+  if questions.riddle_three == true
+    erb(:output)
   else erb(:fail)
   end
-
 end
